ae8fb76f27f418a3309558a3d3782b47d3e104e0
Erik Kostashuk <erikkostashuk@gmail.com>
2021-01-10 12:01:00 -0500

    Update lab10.c

--- lab10.c
+++ lab10.c
@@ -1,20 +1,3 @@
-/*********************************************
- *
- *               Lab10
- *
- *  This is an example of packet sniffing using
- *  the libpcap library.  For packet structures
- *  this program uses the definitions found in
- *  the /usr/include/netinet directory
- *
- *  Without parameters this program will use
- *  the first network interface.  The program
- *  will only work correctly if this is an
- *  ethernet interface.  It won't work for
- *  WiFi.  If there is a parameter it is the name
- *  of a PCAP file that is used for the
- *  packets instead of the network interface.
- *******************************************/
 #include <stdio.h>
 #include <time.h>
 #include <pcap.h>
@@ -34,9 +17,6 @@ int main(int argc, char **argv) {
 	int packet_count = 25;
 	int packet_timeout = 10000;
 
-	/*
-	 *  determine where the packets are coming from
-	 */
 	if(argc == 2) {
 		handle = pcap_open_offline(argv[1], error_buffer);
 		if(handle == NULL) {
@@ -57,65 +37,43 @@ int main(int argc, char **argv) {
 		}
 	}
 
-	/*
-	 *  start the capture loop
-	 */
 	pcap_loop(handle, packet_count, packetCallback, NULL);
 
-	/*
-	 *  close the handle and exit
-	 */
 	pcap_close(handle);
 }
 
 
-/*
- *  This procedure processes a single IP packet.
- *  It does the basic work that you will expand upon.
- *  For the assignment it will call procedures for
- *  handling TCP and UDP packets.
- */
 void processIP(const u_char *packet) {
 	struct iphdr *ip;
 	u_char *payload;
 	char *addr;
 	unsigned int len;
 
-	/*
-	 *  cast the bytes to an IP packet header
-	 */
 	ip = (struct iphdr*) packet;
-	/*
-	 *  check that we have an IPv4 packet
-	 */
+
 	if(ip->version != 4) {
 		printf("not version 4\n");
 		return;
 	}
-	/*
-	 *  compute the header length and the location
-	 *  of the TCP or UDP packet
-	 */
+
 	len = ip->ihl*4;
 	printf("header length: %d\n", len);
 	payload = (unsigned char*)packet+len;
 
 	if(ip->protocol == IPPROTO_TCP) {
 		printf("TCP packet\n");
-		// call the TCP procedure here
+		
 	}
 	if(ip->protocol == IPPROTO_UDP) {
 		printf("UDP packet\n");
-		// call the UDP procedure here
+		
 	}
 
 	printf("time to live: %d \n", ip->ttl);
 	printf("id: %d \n", ip->id);
 	printf("frag: %d \n", ip->frag_off);
 
-	/*
-	 *  print the source and destination addresses
-	 */
+
 	addr = (char*) &(ip->saddr);
 	printf("  source: %hhu.%hhu.%hhu.%hhu\n",addr[0], addr[1],addr[2], addr[3]);
 	addr = (char*) &(ip->daddr);
@@ -124,11 +82,6 @@ void processIP(const u_char *packet) {
 }
 
 
-/*
- *  This is the callback procedure that processes the
- *  ethernet packets.  If the payload is an IP packet
- *  processIP() is called to process it.
- */
 void packetCallback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
 	struct ether_header *eptr;
 	short type;
